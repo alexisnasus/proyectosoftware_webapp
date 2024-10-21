357e25051ecd6ac7435aa1bc4355efac25721471
alexisnasus <lexislema10@gmail.com>
2024-10-21 16:29:57 -0300

    list productos con existencias y barra de busqueda funcionando

--- inv/views.py
+++ inv/views.py
@@ -7,15 +7,20 @@ from .models import Producto
 
 class ProductoListView(ListView):
     model = Producto
-    template_name = 'producto_list.html'  # Ajustamos la ruta del template
+    template_name = 'producto_list.html'
     context_object_name = 'productos'
+    
+    def get_queryset(self):
+        # Utilizar prefetch_related para optimizar las consultas
+        return Producto.objects.all().prefetch_related('inventarios')
 
 def buscar_productos(request):
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
-        query = request.GET.get('term', '')
+        query = request.GET.get('term', '').strip()
         productos = Producto.objects.filter(
             Q(nombre__icontains=query) | Q(codigo_barras__icontains=query)
-        )
+        ).distinct()
+
         results = []
         for producto in productos:
             producto_json = {
@@ -23,6 +28,7 @@ def buscar_productos(request):
                 'nombre': producto.nombre,
                 'codigo_barras': producto.codigo_barras,
                 'precio': str(producto.precio),
+                'stock': producto.total_stock(),  # Añadimos las existencias
             }
             results.append(producto_json)
         return JsonResponse(results, safe=False)
