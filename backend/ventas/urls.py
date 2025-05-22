c4dfb159bf8abde7f128c1c3cab2e9176c927c00
Soundsur <sebastian.alonzo@mail.udp.cl>
2025-05-22 12:28:09 -0400

    Dockerizacion HU02 Sprint 2 #5

--- backend/ventas/urls.py
+++ backend/ventas/urls.py
@@ -3,7 +3,11 @@ from rest_framework.routers import DefaultRouter
 from .views import ProductoViewSet, TransaccionViewSet
 
 router = DefaultRouter()
-router.register('productos', ProductoViewSet, basename='producto')
-router.register('transacciones', TransaccionViewSet, basename='transaccion')
+# router.register('productos', ProductoViewSet, basename='producto')
+# router.register('transacciones', TransaccionViewSet, basename='transaccion')
+
+# urls.py
+router.register(r'productos', ProductoViewSet, basename='producto')
+
 
 urlpatterns = router.urls
